a63a8bc720ecf832c2c6a8f002ac38f510c30a86
DESKTOP-RQ6KKT7\Virtus <lexuanhoang1997@gmail.com>
2017-12-19 00:54:01 +0700

    fullDb

---
+++ XHOnlineShop.Model/Models/OrderDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using XHOnlineShop.Model.Models;
+
+namespace TeduShop.Model.Models
+{
+    [Table("OrderDetails")]
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderID { set; get; }
+
+        [Key]
+        public int ProductID { set; get; }
+
+        public int Quantitty { set; get; }
+
+        [ForeignKey("OrderID")]
+        public virtual Order Order { set; get; }
+
+        [ForeignKey("ProductID")]
+        public virtual Product Product { set; get; }
+    }
+}
