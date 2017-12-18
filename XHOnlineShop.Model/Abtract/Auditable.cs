69dab685b4e213ba1522840bea707aa57ef8f947
DESKTOP-RQ6KKT7\Virtus <lexuanhoang1997@gmail.com>
2017-12-19 00:47:02 +0700

    updatedb_1

---
+++ XHOnlineShop.Model/Abtract/Auditable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace XHOnlineShop.Model.Abtract
+{
+    public class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { get; set; }
+
+        [MaxLength(256)]
+        public string CreatedBy { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        [MaxLength(256)]
+        public string UpdatedBy { get; set; }
+        [MaxLength(256)]
+        public string MetaKeyword { get; set; }
+        [MaxLength(256)]
+        public string MetaDescription { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ XHOnlineShop.Model/Models/ProductCategory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using XHOnlineShop.Model.Abtract;
+
+namespace XHOnlineShop.Model.Models
+{
+    [Table("ProductCategories")]
+    public class ProductCategory : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Alias { get; set; }
+        public string Description { get; set; }
+        public int ParentID { get; set; }
+        public int DisplayOrder { get; set; }
+        public string Image { get; set; }
+        public bool HomeFlag { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
